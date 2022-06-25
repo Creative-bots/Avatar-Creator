488d5e829e0dfa1afe1c0b380ed4b5e4827d6800
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-06-25 03:45:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from discord.ext import commands
 bot =  commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 async def load_cogs():
-print('Loading cogs...')
-for file in os.listdir(os.getcwd() + '/cogs'):
-if file.endswith('.py') and file != '__init__.py':
-await bot.load_extension('cogs.' + file[:-3])
-print(f'Loaded {file}')
+	print('Loading cogs...')
+	for file in os.listdir(os.getcwd() + '/cogs'):
+		if file.endswith('.py') and file != '__init__.py':
+			await bot.load_extension('cogs.' + file[:-3])
+			print(f'Loaded {file}')
 
 
 @bot.command()
 async def ping(ctx):
-await ctx.send(f'Pong! 🏓 {bot.latency * 1000}')
+	await ctx.send(f'Pong! 🏓 {bot.latency * 1000}')
 
 @bot.event
 async def on_ready():
-await load_cogs()
-print(f'Connected to Discord as {bot.user}')
+	await load_cogs()
+	print(f'Connected to Discord as {bot.user}')
 
 bot.run(os.environ.get('DISCORD_TOKEN'))
